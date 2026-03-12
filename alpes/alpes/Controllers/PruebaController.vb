ac5269df53e09a972172a743b4264f93caf43d4b
andres-c0 <andrescabrera854x@gmail.com>
2026-03-11 22:00:09 -0600

    Agregar proyecto inicial al repositorio

---
+++ alpes/alpes/Controllers/PruebaController.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Web.Mvc
+Imports Oracle.ManagedDataAccess.Client
+Imports alpes.Datos
+
+Public Class PruebaController
+    Inherits Controller
+
+    Function Conexion() As ActionResult
+        Dim mensaje As String
+
+        Try
+            Dim conexionBD As New ConexionOracle()
+
+            Using conn As OracleConnection = conexionBD.ObtenerConexion()
+                conn.Open()
+                mensaje = "Conexión exitosa a Oracle."
+            End Using
+
+        Catch ex As Exception
+            mensaje = "Error al conectar con Oracle: " & ex.Message
+        End Try
+
+        ViewBag.Mensaje = mensaje
+        Return View()
+    End Function
+
+End Class
---
+++ alpes/alpes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="alpes.MvcApplication" Language="VB" %>
